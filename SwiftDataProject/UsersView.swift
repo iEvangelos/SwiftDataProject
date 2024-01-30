ec6fb10be2c957e7fc305768345920fe4073170e
Evan Pipis <ievangelos@icloud.com>
2024-01-30 13:49:24 +0000

    Part2: Lesson 2

--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -9,18 +9,43 @@ import SwiftData
 import SwiftUI
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 6)
+                    .padding(.vertical, 3)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(Circle())
+            }
         }
+        .onAppear(perform: addSample)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate > minimumJoinDate
         }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Rob Brydon", city: "Cardiff", joinDate: .now)
+        let job1 = Job(name: "Host Show", priority: 5)
+        let job2 = Job(name: "Arrange haircut", priority: 3)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
