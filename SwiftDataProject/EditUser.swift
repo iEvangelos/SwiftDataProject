2e95a8c242bef8a9d4fedc56949f61e52a837981
Evan Pipis <ievangelos@icloud.com>
2024-01-30 10:57:14 +0000

    Part 1: Lesson 2

---
+++ SwiftDataProject/EditUser.swift
@@ -0,0 +1,18 @@
+//
+//  EditUser.swift
+//  SwiftDataProject
+//
+//  Created by Evangelos Pipis on 30/01/2024.
+//
+
+import SwiftUI
+
+struct EditUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUser()
+}
---
+++ SwiftDataProject/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Evangelos Pipis on 30/01/2024.
+//
+
+import Foundation
