4c6f3fad4b3be0c0930d629f78f1e0d021ae3124
Evan Pipis <ievangelos@icloud.com>
2024-01-30 11:15:15 +0000

    Part 1

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Evangelos Pipis on 30/01/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
